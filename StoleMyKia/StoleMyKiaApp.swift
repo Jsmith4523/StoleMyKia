cd24f7fa73f9c4e127d9331332ab0eacfaa46bf9
Jaylen Smith <kobe26945@icloud.com>
2023-03-11 01:20:31 -0500

    Initial Commit

---
+++ StoleMyKia/StoleMyKiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoleMyKiaApp.swift
+//  StoleMyKia
+//
+//  Created by Jaylen Smith on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct StoleMyKiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
