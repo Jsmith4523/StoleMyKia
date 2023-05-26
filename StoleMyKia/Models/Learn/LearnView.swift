d308ab8751e21fde48da7cfdb4ebc98da00f4d7c
Jaylen Smith <jaylensmith4521@gmail.com>
2023-05-26 03:00:43 -0400

    Learning views

---
+++ StoleMyKia/Models/Learn/LearnView.swift
@@ -0,0 +1,20 @@
+//
+//  LearnView.swift
+//  StoleMyKia
+//
+//  Created by Jaylen Smith on 5/26/23.
+//
+
+import SwiftUI
+
+struct LearnView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LearnView_Previews: PreviewProvider {
+    static var previews: some View {
+        LearnView()
+    }
+}
--- StoleMyKia/StoleMyKiaApp.swift
+++ StoleMyKia/StoleMyKiaApp.swift
@@ -80,22 +80,22 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
 //MARK: DEVELOPER MESSAGE
 
 /*
-    This applciation is dedicated to my 2017 Hyundai Elantra SE that I named "Silvey"
+    This application is dedicated to my 2017 Hyundai Elantra SE that I named "Silvey"
  
     I'm exhausted of this 'Kia Boyz' group stealing peoples cars and trashing them later on. For my car which I wanted to keep forever,
-    I am annoyed by what happened to it and cops not holding these theives up to a proper ass whoopings and punishment.
+    I am annoyed by what happened to it and cops not holding these thieves up to a proper ass whoopings and punishment.
     From the video I took of me approaching the thieves stealing my car and making the dumbass decision to share it with the world, I am also annoyed by social media for making fun of my pain you can hear in the video.
-    Calling me words such as a p-word, that I deseved to have my car stolen for the way I acted and I should've fought and/or shoot the people who were taking my car, these people I despise of as they have nothing better to do but bring down a person who just had their car stolen and now more financial stress from their parent having a medical accident in their vehicle and totaling it a month before having my vehicle stolen. A big "Thank you" and "F*ck you" to those instagram users, and the DMV Media site (not killmoe) for not removing the video as I requested it. Your words fueled me during the creation of this application. I guess you all serve some purpose of my life you f*cking assholes.
-    I hope if you all see this one day have a f*cking stroke trying to read basic SwiftUI syntax and eat sh*t from seeing the amount of files that are in this project for y'all's ungrateful and stealing content asses. deinit this rant, b*tch.
+    Calling me words such as a p-word, that I deserved to have my car stolen for the way I acted and I should've fought and/or shoot the people who were taking my car, these people I despise of as they have nothing better to do but bring down a person who just had their car stolen and now more financial stress from their parent having a medical accident in their vehicle and totaling it a month before having my vehicle stolen. A big "Thank you" and "Fuck you" to those instagram users, and the DMV Media site for not removing the video as I requested it. Your words fueled me during the creation of this application. I guess you all serve some purpose of my life you fucking assholes.
+    I hope if you all see this one day have a fucking stroke from trying to read basic SwiftUI syntax and eat shit from seeing the amount of files that are in this project for y'all's ungrateful and stealing content asses.
  
     I have a fucking Nissan Altima now.
  
     Only people who served little purpose in life find a car stealing TikTok challenging amusing.
  
     This application, while may not do it's job possibly, is my give back to people who take pride in being roll model citizens.
-    This application is not a replacement for acutal authority and law; yet more of a missing-child-on-a-milk-carton type application.
+    This application is not a replacement for atutal authority and law; yet more of a missing-child-on-a-milk-carton type application.
     But the child isn't Asian, it's KDM.
-    My car which I once loved is gone. Yeah it may just be metal and glass, but I absolutely loved that car.
+    My car I once loved is gone. Yeah it may just be metal and glass, but I absolutely loved that car.
     Hoping one day I can find it in the future and fix it up. It may honestly become junkyard parts TBH.
     So here's my solution to the problem. RIP Silvey. Thanks...
  
