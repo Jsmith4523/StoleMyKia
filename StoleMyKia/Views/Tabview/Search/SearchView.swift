a9d6a4b93010239dcbbe991f39ff739b91e456f7
Jaylen Smith <kobe26945@icloud.com>
2023-07-16 02:58:12 -0400

    SearchView and LicensePlateScannerView

---
+++ StoleMyKia/Views/Tabview/Search/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  StoleMyKia
+//
+//  Created by Jaylen Smith on 7/15/23.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
